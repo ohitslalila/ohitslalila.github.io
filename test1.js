79275608cb8c1b8176dc0bfc41078e8448d81b69
lalila_d <lovehulululu@hotmail.com>
2019-03-08 18:17:36 +0800

    Update test1.js

--- test1.js
+++ test1.js
@@ -10,6 +10,11 @@
     var t = document.createTextNode("your BMI is ");
     x.appendChild(t);
     document.body.appendChild(x);
+    var txt1="<p>Text.</p>";               // 以 HTML 创建新元素
+    var txt2=$("<p></p>").text("Text.");   // 以 jQuery 创建新元素
+    var txt3=document.createElement("p");  // 以 DOM 创建新元素
+    txt3.innerHTML="Text.";
+    $("p").append(txt1,txt2,txt3);         // 追加新元素
  
     //获取表单所有name=keyword的值
     // var formData = e.detail.value.;
