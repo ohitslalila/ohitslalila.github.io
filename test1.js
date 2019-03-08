4f766cf2e4ecf5ceaf60407d477ad14372dcff45
lalila_d <lovehulululu@hotmail.com>
2019-03-08 18:08:57 +0800

    update

--- test1.js
+++ test1.js
@@ -1,51 +1,52 @@
-const app = getApp()
-Page({
-  data: {},
- 
   //执行点击事件
-  formSubmit: function (e) {
+  function formSubmit (e) {
  
     //声明当天执行的
     var that = this;
+
+    var x =document.createElement("P");
+    x.setAttribute("id","BMI");
+    var t = document.createTextNode("your BMI is ");
+    x.appendChild(t);
+    document.body.appendChild(x);
  
     //获取表单所有name=keyword的值
-    var formData = e.detail.value.keyword;
+    // var formData = e.detail.value.;
  
-    //显示搜索中的提示
-    wx.showLoading({
-      title: '搜索中',
-      icon: 'loading'
-    })
+    // //显示搜索中的提示
+    // wx.showLoading({
+    //   title: '搜索中',
+    //   icon: 'loading'
+    // })
  
-    //向搜索后端服务器发起请求
-    wx.request({
+    // //向搜索后端服务器发起请求
+    // wx.request({
  
-      //URL
-      url: 'http://localhost/search.php?keyword=' + formData,
+    //   //URL
+    //   url: 'http://localhost/search.php?keyword=' + formData,
  
-      //发送的数据
-      data: formData,
+    //   //发送的数据
+    //   data: formData,
  
-      //请求的数据时JSON格式
-      header: {
-        'Content-Type':'application/json'
-      },
+    //   //请求的数据时JSON格式
+    //   header: {
+    //     'Content-Type':'application/json'
+    //   },
  
-      //请求成功
-      success: function (res) {
+    //   //请求成功
+    //   success: function (res) {
  
-        //控制台打印（开发调试用）
-        console.log(res.data)
+    //     //控制台打印（开发调试用）
+    //     console.log(res.data)
  
-        //把所有结果存进一个名为re的数组
-        that.setData({
-          re: res.data,
-        })
+    //     //把所有结果存进一个名为re的数组
+    //     that.setData({
+    //       re: res.data,
+    //     })
  
-        //搜索成功后，隐藏搜索中的提示
-        wx.hideLoading();
-      }
-    })
-  },
-})
+    //     //搜索成功后，隐藏搜索中的提示
+    //     wx.hideLoading();
+    //   }
+    // })
+  }
 
