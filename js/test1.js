4533b650e71c2ff4c86320adcf50c75fad3af8c4
lalila_d <lovehulululu@hotmail.com>
2019-03-10 21:40:56 +0800

    1st

---
+++ js/test1.js
@@ -0,0 +1,24 @@
+$("#btn").click(function(){
+
+    $.ajax({
+        url:'http://162.105.94.242/bdtest.php',
+        type:'post',
+        dataType:'json',
+        data:{post1:$("#name_input").val(), post2:$("#num_input").val()},
+        success:function(data){
+                // $("#result").text(JSON.stringify(data));
+                // $("#result").text("!");
+                for (var key in data) {
+					if (key.search("第") != -1 || key.search("error") != -1){
+                    $("#grade").append("<li>" + key + ": " + data[key] + "</li>");
+                };
+                };
+                // $("#post2").text(data[1]);
+        },
+        error:function(data){
+        	    $("#result").text("error: " + data);
+                // console.log(data);
+
+        },
+    });
+});
