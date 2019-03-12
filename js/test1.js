f6a45fd2363e7cd035058c7bb6258c0f19afcafb
lalila_d <lovehulululu@hotmail.com>
2019-03-12 16:45:40 +0800

    test 1

--- js/test1.js
+++ js/test1.js
@@ -2,15 +2,21 @@ $("#btn").click(function(){
 
     $.ajax({
         url:'https://162.105.94.242/bdtest.php',
-        type:'post',
+        type:'get',
         dataType:'jsonp',
         data:{post1:$("#name_input").val(), post2:$("#num_input").val()},
         success:function(data){
                 // $("#result").text(JSON.stringify(data));
                 // $("#result").text("!");
                 for (var key in data) {
-					if (key.search("第") != -1 || key.search("error") != -1){
-                    $("#grade").append("<li>" + key + ": " + data[key] + "</li>");
+					if (key.search("error") != -1){
+                    $("#grade").text("<li>" + key + ": " + data[key] + "</li>");
+                } else { 
+                    $("#grade").text("<li>" + "查询结果: " + "</li>");          
+	                for (var key in data) {
+						if (key.search("第") != -1){
+	                    $("#grade").append("<li>" + key + ": " + data[key] + "</li>");
+	                };
                 };
                 };
                 // $("#post2").text(data[1]);
